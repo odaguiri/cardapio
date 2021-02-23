b993d0232629a21a4f54c2a1e94342048ab4da6a
Raul Odaguiri <raulodaguiri@gmail.com>
2021-02-22 23:57:52 -0300

    Remove unused plugins from auth service

--- auth_service/auth_service.rb
+++ auth_service/auth_service.rb
@@ -1,57 +1,73 @@
-require 'roda'
-require 'puma'
+# core
 require 'pg' 
 require 'logger'
+require 'mail'
+require 'securerandom'
+
+# bundle
+require 'roda'
+require 'puma'
 require 'sequel/core'
 
 module AuthService
 class App < Roda
+  database_name = ENV.delete('DATABASE_NAME')
+  secret = ENV.delete('SESSION_SECRET')
+
   DB = Sequel.connect(ENV.delete('DATABASE_URL'))
   DB.loggers << Logger.new($stdout)
-  DB.freeze
+
+  ::Mail.defaults do
+    delivery_method :test
+  end
 
   opts[:root] = File.dirname(__FILE__)
 
-  plugin :render, escape: true
+  MAILS = {}
+  MUTEX = Mutex.new
+
   plugin :hooks
-  plugin :flash
   plugin :common_logger
-  plugin :route_csrf
-
-  secret = ENV.delete('SESSION_SECRET')
-  plugin :sessions, :secret=>secret, :key=>'auth.session'
-
-  plugin :rodauth, :json=>true, :csrf=>:route_csrf do
+  plugin :rodauth, :json=>true do
     db DB
-    enable :change_login, :change_password, :close_account, :create_account,
-           :lockout, :login, :logout, :remember, :reset_password, :verify_account,
-           :otp, :recovery_codes, :sms_codes, :disallow_common_passwords,
-           :disallow_password_reuse, :password_grace_period, :active_sessions, :jwt,
-           :verify_login_change, :change_password_notify, :confirm_password,
-           :email_auth
-    enable :webauthn, :webauthn_login if ENV['WEBAUTHN']
-    enable :webauthn_verify_account if ENV['WEBAUTHN_VERIFY_ACCOUNT']
+    enable :login, :logout, :create_account, :close_account, :change_password, :lockout, :change_login, :json, :jwt, :email_auth 
+    # if using the disallow_password_reuse feature:
+    # previous_password_hash_table Sequel.lit("#{DATABASE_NAME}_password.account_previous_password_hashes")
+    password_hash_table "#{database_name}_password.account_password_hashes"
+    password_hash_table Sequel[:auth_password][:account_password_hashes]
+    function_name do |name|
+      "#{database_name}_password.#{name}"
+    end
     max_invalid_logins 2
-    account_password_hash_column :ph
-    title_instance_variable :@page_title
-    only_json? false
-    jwt_secret(secret)
+    use_database_authentication_functions? true
+    only_json? true
+    jwt_secret secret
     hmac_secret secret
   end
 
   plugin :error_handler do |e|
-    @page_title = 'Internal Server Error'
-    view :content=>"#{h e.class}: #{h e.message}<br />#{e.backtrace.map{|line| h line}.join("<br />")}"
+    {
+      error: "#{h e.class}: #{h e.message}",
+      detail: e.backtrace.map{|line| h line}.join(",")
+    }
   end
 
   plugin :not_found do
-    @page_title = 'File Not Found'
-    view :content=>''
+    { error: :not_found }
+  end
+
+  def last_mail_sent
+    MUTEX.synchronize{MAILS.delete(rodauth.session_value)}
+  end
+
+  after do
+    Mail::TestMailer.deliveries.each do |mail|
+      MUTEX.synchronize{MAILS[rodauth.session_value] = mail}
+    end
+    Mail::TestMailer.deliveries.clear
   end
 
   route do |r|
-    rodauth.load_memory
-    rodauth.check_active_session
     r.rodauth
   end
   
